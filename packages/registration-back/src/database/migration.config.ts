28419c31d5a1fd506d7c4513eba6ffa2b8de5433
saviosa <saviosilvadearaujo2019@gmail.com>
2022-07-21 15:43:22 -0300

    refactor(migration.config.ts): set envirovment variables to work with migrations and running time

---
+++ packages/registration-back/src/database/migration.config.ts
@@ -0,0 +1,7 @@
+import { DataSource } from 'typeorm';
+import { getConfig } from './ormconfig';
+
+const datasource = new DataSource(getConfig());
+
+datasource.initialize();
+ export default datasource; 
--- packages/registration-back/src/database/ormconfig.ts
+++ packages/registration-back/src/database/ormconfig.ts
@@ -1,18 +1,21 @@
-import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
+import { DataSourceOptions } from 'typeorm';
+dotenv.config({path: '../../.env'});
 
-
-export const connectionSource = new DataSource({
-      migrationsTableName: 'migrations',
-      type: 'mysql',
-      port: 3306,
-      host: process.env.MYSQL_HOST,
-      username: process.env.MYSQL_USER,
-      password: process.env.MYSQL_ROOT_PASSWORD,
-      database:  process.env.MYSQL_DATABASE,
-      logging: false,
-      synchronize: false,
-      name: 'default',
-      entities: ['src/**/**.entity{.ts,.js}'],
-      migrations: ['src/database/migrations/**/*{.ts,.js}'],
-      subscribers: ['src/database/subscriber/**/*{.ts,.js}'],
-   });
+export function getConfig() {
+      return {
+            migrationsTableName: 'migrations',
+            type: 'mysql',
+            port: process.env.MYSQL_PORT,
+            host: process.env.MYSQL_HOST,
+            username: process.env.MYSQL_USER,
+            password: process.env.MYSQL_ROOT_PASSWORD,
+            database:  process.env.MYSQL_DATABASE,
+            logging: false,
+            synchronize: false,
+            name: 'default',
+            entities: ['src/**/**.entity{.ts,.js}'],
+            migrations: ['src/database/migrations/**/*{.ts,.js}'],
+            subscribers: ['src/database/subscriber/**/*{.ts,.js}'],
+      } as DataSourceOptions;
+}
