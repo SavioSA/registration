9aa799686c44123ca1055847b5a5441b18564e20
saviosa <saviosilvadearaujo2019@gmail.com>
2022-07-21 23:51:17 -0300

    feat(controller/user/index.ts): add edit user logic

--- packages/registration-back/src/controllers/user/index.ts
+++ packages/registration-back/src/controllers/user/index.ts
@@ -65,8 +65,34 @@ router.post<{}, MessageInterface, UserInterface >('/',
     };
 });
 
-router.put('/', (req: Request, res: Response) => {
-  
+router.put<{}, MessageInterface, UserInterface>('/',
+  body('name').isLength({min: 3, max: 100}).optional().withMessage('name must have beetwen 3 and 100 characters.'),
+  body('name').notEmpty().optional(),
+  body('name').isString().optional().withMessage('name must be a string.'),
+  body('birthday').isDate().optional().withMessage('birthday must be a date.'),
+  body('birthday').notEmpty().optional(),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);      
+      if (errors.isEmpty()) {
+        const { id } = req.body
+        const userExists = await userRepository.findOne({
+          where: { id }
+        });
+        if (!userExists) {
+          res.status(404).json({msg: `There was an error with your request: User not found.`});
+        } else {
+          userRepository.update({ id }, { ...req.body })
+          res.status(200).json({ msg: 'User updated successfully.' });
+        }
+      } else {
+        const errorMessage = setErrorValidationMessage(errors.array());
+        res.status(403).json({ msg: errorMessage });
+      }
+    
+    } catch (error) {
+      res.status(500).json({ msg: `There was an error with your request: ${error}` });
+    }
 });
 
 router.delete('/', (req: Request, res: Response) => {
