07c8543a1d92360146ecee325ffd4e99d5993eb4
saviosa <saviosilvadearaujo2019@gmail.com>
2022-07-22 16:13:36 -0300

    feat(main.ts): set pinia config

--- packages/registration-front/src/main.ts
+++ packages/registration-front/src/main.ts
@@ -1,12 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
+import router from "./router";
 
-loadFonts()
+loadFonts();
+const pinia = createPinia();
 
-createApp(App)
-  .use(router)
-  .use(vuetify)
-  .mount('#app')
+createApp(App).use(router).use(vuetify).use(pinia).mount("#app");
