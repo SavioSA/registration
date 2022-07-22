a9fff113582881f1939a54662255d132f3c46ee6
saviosa <saviosilvadearaujo2019@gmail.com>
2022-07-22 13:55:31 -0300

    feat(vuetify.ts): install font awesome

--- packages/registration-front/src/plugins/vuetify.ts
+++ packages/registration-front/src/plugins/vuetify.ts
@@ -1,10 +1,16 @@
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
-
+import "@mdi/font/css/materialdesignicons.css";
 // Vuetify
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
+import { aliases, fa } from "vuetify/iconsets/fa";
+import "vuetify/styles";
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+  icons: {
+    defaultSet: "fa",
+    aliases,
+    sets: {
+      fa,
+    },
+  },
+});
