01866c1bd9e8365b2ec827d82247b72afa09960e
saviosa <saviosilvadearaujo2019@gmail.com>
2022-07-23 13:17:53 -0300

    feat(userregistrationview.vue): add UserRegistrationView.vue

---
+++ packages/registration-front/src/views/UserRegistrationView.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="list-section">
+    <UserRegistrationForm />
+  </section>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import UserRegistrationForm from "../components/UserRegistrationForm.vue";
+export default defineComponent({
+  name: "UserRegistrationView",
+  components: {
+    UserRegistrationForm,
+  },
+});
+</script>
+
+<style scoped>
+section.list-section {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+</style>
