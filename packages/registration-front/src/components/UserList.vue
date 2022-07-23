5b64e3b2548d4b16b433f6bf716db1ed84ebb39c
saviosa <saviosilvadearaujo2019@gmail.com>
2022-07-22 21:57:34 -0300

    feat(userlist.vue): paginate user list

--- packages/registration-front/src/components/UserList.vue
+++ packages/registration-front/src/components/UserList.vue
@@ -1,33 +1,48 @@
 <template>
   <section class="main-section">
-    <v-table>
-      <thead>
-        <tr>
-          <th class="text-left">Nome</th>
-          <th class="text-left">Data de nascimento</th>
-          <th class="text-left">Opções</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="user in users" :key="user.name">
-          <td>{{ user.name }}</td>
-          <td>{{ formatDate(user.birthday) }}</td>
-          <td><v-icon icon="fas fa-trash" /></td>
-        </tr>
-      </tbody>
-    </v-table>
+    <section class="table-section">
+      <v-table>
+        <thead>
+          <tr>
+            <th class="text-left">Nome</th>
+            <th class="text-left">Data de nascimento</th>
+            <th class="text-left">Opções</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" :key="user.name">
+            <td>{{ user.name }}</td>
+            <td>{{ formatDate(user.birthday) }}</td>
+            <td><v-icon icon="fas fa-trash" /></td>
+          </tr>
+        </tbody>
+      </v-table>
+    </section>
+    <section class="pagination-section">
+      <v-pagination
+        v-model="currentPage"
+        :length="pagesQuantity"
+      ></v-pagination>
+    </section>
   </section>
 </template>
 <script lang="ts">
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, ref, watch } from "vue";
 import { useUsersStore } from "../stores/users";
 export default defineComponent({
   name: "UserList",
   setup() {
+    const currentPage = ref(1);
+    watch(currentPage, (val) => {
+      usersStore.fetchUsers(8, val);
+    });
     const usersStore = useUsersStore();
     usersStore.fetchUsers();
     const users = computed(() => {
-      return usersStore.getUsers;
+      return usersStore.getUsersInformations.users;
+    });
+    const pagesQuantity = computed(() => {
+      return usersStore.getUsersInformations.pagesQuantity;
     });
     const formatDate = (value) => {
       if (value) {
@@ -41,6 +56,8 @@ export default defineComponent({
     return {
       users,
       formatDate,
+      currentPage,
+      pagesQuantity,
     };
   },
 });
@@ -54,5 +71,9 @@ section.main-section {
   section.main-section {
     width: 70%;
   }
+}
+
+section.table-section {
+  min-height: 27.25rem;
 }
 </style>
