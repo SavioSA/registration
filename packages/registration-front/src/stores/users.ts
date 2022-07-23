093b9681be1b735dd7a882230ade522c447cd225
saviosa <saviosilvadearaujo2019@gmail.com>
2022-07-23 15:52:28 -0300

    feat(stores/users.ts): add function to create new user

--- packages/registration-front/src/stores/users.ts
+++ packages/registration-front/src/stores/users.ts
@@ -1,4 +1,7 @@
-import type { UsersPaginationInterface } from "@lib/utilInterfaces";
+import type {
+  UserInterface,
+  UsersPaginationInterface
+} from "@lib/utilInterfaces";
 import axios from "axios";
 import { defineStore } from "pinia";
 
@@ -6,11 +9,14 @@ export const useUsersStore = defineStore({
   id: "user",
   state: (): {
     usersInformations: UsersPaginationInterface | undefined;
+    currentUser: UserInterface | undefined;
   } => ({
     usersInformations: undefined,
+    currentUser: undefined,
   }),
   getters: {
     getUsersInformations: (state) => state.usersInformations,
+    getCurrentUser: (state) => state.currentUser,
   },
   actions: {
     async fetchUsers(offset = 8, page = 0) {
@@ -19,6 +25,18 @@ export const useUsersStore = defineStore({
           `http://localhost:3333/users?offset=${offset}&page=${page}`
         );
         this.usersInformations = query.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async createUser(user: UserInterface) {
+      try {
+        console.log("asdadasd");
+
+        const query = await axios.post(`http://localhost:3333/users`, {
+          ...user,
+        });
+        this.currentUser = query.data;
       } catch (error) {
         console.error(error);
       }
