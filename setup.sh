bd751586bf9c4690c3de27aa4259f4d5e7f023f0
saviosa <saviosilvadearaujo2019@gmail.com>
2022-07-24 19:43:50 -0300

    feat(setup.sh): stop containers before run

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 
 cp .env.example .env
 
+echo "Stopping current containers..."
+docker stop $(docker ps -a -q)
+docker-compose rm -fs mysql front back
+
 docker-compose up --build -d
 
 
@@ -13,7 +17,6 @@ if ! yarn -v /etc/hosts; then
 fi
 
 cd packages/registration-back
-yarn install
 yarn run migration:up
 
 echo registration is ready on http://localhost:5173
